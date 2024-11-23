05855b04ac9d77c7b9ca3cedb893de7a920246ad
RahulBalakrish <144721721+RahulBalakrish@users.noreply.github.com>
2024-11-22 23:31:05 -0500

    Initial Commit

---
+++ final_project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final_project
+//
+//  Created by Rahul B on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ final_projectTests/final_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectTests.swift
+//  final_projectTests
+//
+//  Created by Rahul B on 11/22/24.
+//
+
+import Testing
+@testable import final_project
+
+struct final_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
